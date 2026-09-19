c9e2969550a58daf1498cdc44ee857c026da794f
J-BandaVT <jordanb06@vt.edu>
2026-09-19 09:32:45 -0400

    Testing google gemini API import

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
 import os
 from flask import Flask, jsonify, request
-from dotenv import load_dotenv
+from dotenv import load_dotenv #read key value pairs from .env file and set them as environment variables
+from google import genai
+
