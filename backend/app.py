7c4dac91e0ca43345ca97f9a80da3dd8d4c83938
J-BandaVT <jordanb06@vt.edu>
2026-09-19 18:36:53 -0400

    Add render compliant files (.env, render.yaml, wsgi.py) render.yaml will format for render cloud hosting, contains required start and build commands. wsgi pulls for the application and runs through gunicorn web server for python. .env remains empty until next commit call.

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,12 @@ from flask import Flask, jsonify
 from dotenv import load_dotenv
 import sys, pathlib
 
-sys.path.append(str(pathlib.Path(__file__).resolve().parent.parent))
+BASE_DIR = pathlib.Path(__file__).parent.resolve()
+PROJECT_ROOT = BASE_DIR.parent
+sys.path.append(str(PROJECT_ROOT))
 from routes.ai_routes import ai_bp
 load_dotenv()
+
 app = Flask(__name__)
 app.register_blueprint(ai_bp)
 
@@ -16,3 +19,5 @@ def handle_exception(e):
     return jsonify({"error": str(e)}), 500
 
 #testing: test if we can push to the github
+if __name__ == "__main__":
+    app.run(debug=True)
